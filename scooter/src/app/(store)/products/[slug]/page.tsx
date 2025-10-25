94e3e75ab3e452132eae3bf1e73ce52cf3839d6c
samsonmeseret <samsonmeseretb@gmail.com>
2025-10-25 13:10:06 +0300

    (feat): alignment fixes

--- scooter/src/app/(store)/products/[slug]/page.tsx
+++ scooter/src/app/(store)/products/[slug]/page.tsx
@@ -1,37 +1,28 @@
-
-import { notFound } from "next/navigation"
-import { getProductBySlug } from "@/lib/services/product.service" // Your database service
-import ProductDetailClient from "./productDetailClient" 
+import { notFound } from "next/navigation";
+import { getProductBySlug } from "@/lib/services/product.service"; // Your database service
+import ProductDetailClient from "./productDetailClient";
 
 interface Props {
-  params: { slug: string }
+    params: { slug: string };
 }
 
 export default async function ProductDetailPage({ params }: Props) {
-  
-  const {slug}= await params
-  const data = await getProductBySlug(slug)
+    const { slug } = await params;
+    const data = await getProductBySlug(slug);
 
-  if (!data) return notFound()
-  
+    if (!data) return notFound();
 
-  const { product, category, relatedProduct } = data;
+    const { product, category, relatedProduct } = data;
 
- 
-  return (
-    <div className="flex min-h-screen flex-col">
-      
-
-      <main className="flex-1">
-       
-        <ProductDetailClient 
-          product={product} 
-          category={category} 
-          relatedProducts={relatedProduct} 
-        />
-      </main>
-
-      
-    </div>
-  )
+    return (
+        <div className="flex min-h-screen  flex-col">
+            <main className="flex-1">
+                <ProductDetailClient
+                    product={product}
+                    category={category}
+                    relatedProducts={relatedProduct}
+                />
+            </main>
+        </div>
+    );
 }
