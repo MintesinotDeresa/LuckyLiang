926b466abd80eb856d87a7fd8ebcf433c9e7865a
samsonmeseret <samsonmeseretb@gmail.com>
2025-11-04 12:43:17 +0300

    (feat) : google add added

--- scooter/src/app/layout.tsx
+++ scooter/src/app/layout.tsx
@@ -1,19 +1,23 @@
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata = {
-  title: "Lucky Liang E-Scooters",
-  description: "Premium E-Scooters in Dubai",
+    title: "Lucky Liang E-Scooters",
+    description: "Premium E-Scooters in Dubai",
     icons: {
-    icon: "/favicon.ico", 
-  },
-
+        icon: "/favicon.ico",
+    },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+            <GoogleAnalytics gaId="AW-17700462107" />
+        </html>
+    );
 }
